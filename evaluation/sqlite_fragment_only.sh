eb5e74008bfeff39d91edc541b63516020580dad
Jun Lu <120633988+YeDonS@users.noreply.github.com>
2025-11-13 13:43:00 -0500

    Add files via upload

--- evaluation/sqlite_fragment_only.sh
+++ evaluation/sqlite_fragment_only.sh
@@ -15,6 +15,10 @@ EXP_LAMBDA=${EXP_LAMBDA:-0.0008}
 EXP_SEED=${EXP_SEED:-4242}
 RUN_ZIPF=${RUN_ZIPF:-1}
 RUN_EXP=${RUN_EXP:-1}
+RUN_NORMAL=${RUN_NORMAL:-0}
+NORMAL_MEAN=${NORMAL_MEAN:-5000}
+NORMAL_STDDEV=${NORMAL_STDDEV:-800}
+NORMAL_SEED=${NORMAL_SEED:-314159}
 
 run_fragment_suite() {
     local mode=$1
@@ -35,6 +39,8 @@ run_fragment_suite() {
             --mode read --distribution zipf --reads "$READ_QUERIES" \
             --alpha "$ZIPF_ALPHA" --seed "$ZIPF_SEED" \
             --log "./$RESULT_FOLDER/sqlite_${tag}_zipf_trace.csv" \
+            --heatmap "./$RESULT_FOLDER/sqlite_${tag}_zipf_heat.csv" \
+            --human-log \
             > "./$RESULT_FOLDER/sqlite_${tag}_zipf.txt"
     fi
 
@@ -44,7 +50,21 @@ run_fragment_suite() {
             --mode read --distribution exp --reads "$READ_QUERIES" \
             --lambda "$EXP_LAMBDA" --seed "$EXP_SEED" \
             --log "./$RESULT_FOLDER/sqlite_${tag}_exp_trace.csv" \
+            --heatmap "./$RESULT_FOLDER/sqlite_${tag}_exp_heat.csv" \
+            --human-log \
             > "./$RESULT_FOLDER/sqlite_${tag}_exp.txt"
+    fi
+
+    if [[ $RUN_NORMAL -eq 1 ]]; then
+        sync; echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null
+        numactl --cpubind=$NUMADOMAIN --membind=$NUMADOMAIN ./sqlite_append \
+            --mode read --distribution normal --reads "$READ_QUERIES" \
+            --normal-mean "$NORMAL_MEAN" --normal-stddev "$NORMAL_STDDEV" \
+            --seed "$NORMAL_SEED" \
+            --log "./$RESULT_FOLDER/sqlite_${tag}_normal_trace.csv" \
+            --heatmap "./$RESULT_FOLDER/sqlite_${tag}_normal_heat.csv" \
+            --human-log \
+            > "./$RESULT_FOLDER/sqlite_${tag}_normal.txt"
     fi
 
     source resetdevice.sh
